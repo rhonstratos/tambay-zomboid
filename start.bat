8e74bd85bbbb4cb3a622a79af4ce5433df0b03aa
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2025-12-25 00:50:08 +0800

    fix: comments

--- start.bat
+++ start.bat
@@ -7,15 +7,6 @@
 
 @REM if you are planning to use beta features, set STEAM_USER and STEAM_PASSWORD accordingly
 
-@REM Start the Project Zomboid server in a Docker container
-@REM Make sure to build the Docker image first using build.bat
-@REM Then run this script to start the server container
-@REM Adjust the volume path and environment variables as needed
-@REM This script maps the necessary ports and mounts the Zomboid data directory
-@REM to persist server data
-
-@REM if you are planning to use beta features, set STEAM_USER and STEAM_PASSWORD accordingly
-
 @echo off
 docker run -d --name pz1 ^
   -p 8766:8766/udp ^
